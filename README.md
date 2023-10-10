3a6cb070d10537513a4783c5d8a320b27dffb0ba
tetyanaleleka <tana.leleka@gmail.com>
2023-10-10 19:28:06 +0100

    The structure of README file added

--- README.md
+++ README.md
@@ -1,1 +1,53 @@
-# module11-challenge
+# Module 11
+  ![Github license](https://img.shields.io/badge/license-MIT-blue.svg)
+
+  ### Description
+
+  Welcome to the README file for the README Generator App. Below you will find important information about the app that generates strong README file, technologies used, and instructions on how to run or contrubute to the app
+
+  ### Table of Contents
+
+  * [Installation](#installation)
+
+  * [Usage](#usage)
+
+  * [License](#license)
+
+  * [Contributing](#contributing)
+
+  * [Test](#tests)
+
+  * [Questioins](#questions)
+
+  ## Installation
+
+  To install necessary dependencies, run the following command:
+
+  ```
+  npm
+  ```
+
+  ## Usage
+  The README file Generator App shows: the name of the project, description, the type of license and its installation.
+
+  ## License
+
+  This project is licensed under the MIT license.
+
+  ## Contributing
+
+  The team of collaborators: University of Burmingham students (Bootcamp)
+
+  ## Test
+
+  To run tests, run following command:
+
+  ```
+   Mit
+  ```
+  
+  ## Questions
+
+  If you have any questioins about the repo open an issue or contact me directly at tana.leleka@gmail.com.
+
+  You can find more of my work at [tetyanaleleka](https://github.com/tetyanaleleka).
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,14 +1,72 @@
 // function to generate markdown for README
-function renderLicenseBadge(license){
+const renderLicenseBadge = (license) => {
   if (license !== "None") {
     return `![Github license](https://img.shields.io/badge/license-${license}-blue.svg)`
   }
+  return '';
+}
+
+const renderLicenseLink = (license) => {
+  if(license !== "None") {
+    return `* [License](#License)\n`
+  }
+  return '';
 }
 function generateMarkdown(data) {
+  console.log(data);
   return `# ${data.title}
   ${renderLicenseBadge(data.license)}
 
+  ### Description
 
+  ${data.description}
+
+  ### Table of Contents
+
+  * [Installation](#installation)
+
+  * [Usage](#usage)
+
+  * [License](#license)
+
+  * [Contributing](#contributing)
+
+  * [Test](#tests)
+
+  * [Questioins](#questions)
+
+  ## Installation
+
+  To install necessary dependencies, run the following command:
+
+  \`\`\`
+  ${data.installation}
+  \`\`\`
+
+  ## Usage
+  ${data.usage}
+
+  ## License
+
+  This project is licensed under the ${data.license} license.
+
+  ## Contributing
+
+  ${data.contributing}
+
+  ## Test
+
+  To run tests, run following command:
+
+  \`\`\`
+  ${data.test}
+  \`\`\`
+  
+  ## Questions
+
+  If you have any questioins about the repo open an issue or contact me directly at ${data.email}.
+
+  You can find more of my work at [${data.github}](https://github.com/${data.github}).
 `;
 }
 
